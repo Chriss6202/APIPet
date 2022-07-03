671be0a4ab39721a0d654a0807751e004eef9849
Primi0 <chriss6202@gmail.com>
2022-07-03 14:26:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(authRouter);
-app.use("/auth", userRouter);
 app.use("/post", postRouter);
 
 module.exports = app;
