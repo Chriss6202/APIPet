fde33e717aba950639764d4233614789fa48f565
Primi0 <chriss6202@gmail.com>
2022-07-05 19:15:31 -0500

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -21,7 +21,17 @@ exports.SignIn = async (req, res, next) => {
 exports.whoAmI = async (req, res, next) => {
   try{
     token = req.params.token
-    
+
+    function parseJwt (token) {
+      var base64Url = token.split('.')[1];
+      var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''));
+  
+      return JSON.parse(jsonPayload);
+  };
+
   res.send ({
     message: parseJwt(token)
   })
