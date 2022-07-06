d204d95214385af35095f5602be29cfb1b9826fd
Primi0 <chriss6202@gmail.com>
2022-07-05 21:51:06 -0500

    Update post.js

--- controllers/post.js
+++ controllers/post.js
@@ -197,7 +197,6 @@ exports.getAll = async (req, res, next) => {
       let user = getusername(token)
       let post = await PostModel.findById(_id);
       let favsArray = post.favorite.indexOf(user)
-      res.send({message: favsArray})
       if (favsArray > -1)
       {
         post.favorite.splice(favsArray, 1)
